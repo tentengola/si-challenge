83a259c12f448e92a15b82f3a3c0dbddb6dd1c03
tantangula <stephenbgarrett@gmail.com>
2020-01-25 19:44:51 +0000

    adding put and post methods

--- interactions.handler.js
+++ interactions.handler.js
@@ -15,14 +15,25 @@ const put = async (deps, event) => {
   }
 };
 
+const post = async (deps, event) => {
+  try {
+    return response(200, JSON.stringify({message:'Hello'}));
+  } catch(err) {
+    console.log('err', err);
+    throw err;
+  }
+};
+
 exports.methods = deps => async (event, context, callback) => {
   try {
     switch (event.httpMethod) {
       case 'PUT':
         return await put(deps, event);
+      case 'POST':
+        return await post(deps, event);
       default: return response(405,
         { Message: `Unsupported method: ${event.httpMethod}` },
-        { Allow: 'PUT' });
+        { Allow: 'POST,PUT' });
     }
   } catch(err) {
     return response(500, JSON.stringify({ Error: err.message }));
--- tests/interactions.test.js
+++ tests/interactions.test.js
@@ -12,14 +12,32 @@ const handler = methods(deps);
 
 describe('Save an interaction', function() {
   it('returns a response from enpoint', async () => {
-    let response = await handler({
+    const response = await handler({
       httpMethod: 'PUT'
     });
     response.statusCode.should.exist;
+  });
+  it('rejects unsupported methods', async () => {
+    const response = await handler({
+      httpMethod: 'UPDATE'
+    });
+    response.statusCode.should.equal(405);
   });
   it('successfully puts an interaction in the database');
 });
 
 describe('Retrieve interactions', function() {
+  it('returns a response from enpoint', async () => {
+    const response = await handler({
+      httpMethod: 'POST'
+    });
+    response.statusCode.should.exist;
+  });
+  it('rejects unsupported methods', async () => {
+    const response = await handler({
+      httpMethod: 'UPDATE'
+    });
+    response.statusCode.should.equal(405);
+  });
   it('successfully returns interactions from the database');
 });
